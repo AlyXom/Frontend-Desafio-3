4f320cc6d38a9c18b82c8d26f9283dc052fc817e
Alysson <alyssoncraft12@gmail.com>
2024-10-22 16:04:40 -0300

    created pagination component

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -29,7 +29,7 @@ export default function ProductsList() {
     }, [state])
     dispatch(showing(products.length))
     return (
-        <div className="flex justifyCenter" style={{width: "100%", marginTop: "3%"}}>
+        <div className="flex justifyCenter" style={{width: "100%", marginTop: "3%", marginBottom: "5%"}}>
             <Container className="wrap" style={{width: "80%", gap: 30}}>
             {products.map((item) => {
                 return (
