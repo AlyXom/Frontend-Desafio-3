7907a57defcb905d9ffff17cd5c44ec000c14f28
Alysson <alyssoncraft12@gmail.com>
2024-10-22 23:35:47 -0300

    Footer added on the shop page

--- src/routes/Shop.tsx
+++ src/routes/Shop.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import MainShop from "../components/MainShop";
+import Footer from "../components/Footer";
 
 export default function Shop() {
 
@@ -8,6 +9,7 @@ export default function Shop() {
         <div>
             <Header />
             <MainShop />
+            <Footer />
         </div>
     )
 }
