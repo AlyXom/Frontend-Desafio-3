0bd638409bce4a1cc7fc65d4d6ce4a77d4581f1a
Alysson <alyssoncraft12@gmail.com>
2024-10-23 15:43:30 -0300

    refac/Product Navigation

--- src/components/ProductNavigation.tsx
+++ src/components/ProductNavigation.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Container, Icons, Text } from "../styles/styledComponents";
 import "../styles/styles.css"
 import { svgPath } from "./IconsBar";
+import { Product } from "../types/product";
 
-export default function ProductNavigation() {
+export default function ProductNavigation({product}: {product: Product}) {
     return (
         <Container className="alignCenter" style={{backgroundColor: "#F9F1E7", height:70}}>
             <div className="flex w-90 gap alignCenter">
@@ -12,7 +13,7 @@ export default function ProductNavigation() {
                 <Text style={{color: "#9F9F9F"}}>Shop</Text>
                 <Icons $width={13} $height={13} src={`${svgPath}/arrow.svg`}/>
                 <span style={{borderRight: "2px #9F9F9F solid", height: 30}}></span>
-                <Text></Text>
+                <Text style={{marginLeft: 10}}>{product.name}</Text>
             </div>
         </Container>
     )
