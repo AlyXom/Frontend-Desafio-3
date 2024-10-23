7d2d590c67b4d2b57321fa243fb3a3a5c82106ed
Alysson <alyssoncraft12@gmail.com>
2024-10-23 13:59:41 -0300

    feat/Adding header

--- src/routes/SingleProduct.tsx
+++ src/routes/SingleProduct.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { useParams } from "react-router-dom"
+import Header from "../components/Header";
 
 export default function SingleProduct() {
-    const param = useParams()
-
-    console.log(param)
     return (
         <div>
-            <h1>{`Single Product: ${param.id}`}</h1>
+            <Header />
         </div>
     )
 }
